97767397af8383580ee2d29611d67df26aea5baf
Rennie Bevineau <rsbevineau@gmail.com>
2015-11-30 15:07:07 -0700

    add authorization header

--- app/services/powerbi.service.js
+++ app/services/powerbi.service.js
@@ -5,7 +5,7 @@
     function powerbiService(powerBIUrl, $http, $resource) {
 
 
-          $http({method: 'GET', url: 'https://api.powerbi.com/v1.0/myorg/datasets'})
+          $http({method: 'GET', url: 'https://api.powerbi.com/v1.0/myorg/dashboards', headers: {'authorization': 'Bearer tokenString'}})
         .success(function(d){ console.log( "yay" ); })
         .error(function(d){ console.log( "nope" ); });
 
