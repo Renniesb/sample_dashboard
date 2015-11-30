3a050552e4934a54bdda881f9c363f617ac8d527
Rennie Bevineau <rsbevineau@gmail.com>
2015-11-30 13:58:40 -0700

    add json callback

--- app/services/powerbi.service.js
+++ app/services/powerbi.service.js
@@ -5,7 +5,7 @@
     function powerbiService(powerBIUrl, $http, $resource) {
 
 
-          $http({method: 'GET', url: 'https://api.powerbi.com/v1.0/myorg/datasets'})
+          $http.jsonp('https://api.powerbi.com/v1.0/myorg/datasets?callback=JSON_CALLBACK')
         .success(function(d){ console.log( "yay" ); })
         .error(function(d){ console.log( "nope" ); });
 
