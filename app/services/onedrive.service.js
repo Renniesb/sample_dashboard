1cf8b1936827308bec0f8570e84add382372d107
Rennie <rsbevineau@gmail.com>
2016-01-23 16:38:02 -0700

    add code for one drive connection

--- app/services/onedrive.service.js
+++ app/services/onedrive.service.js
@@ -5,7 +5,7 @@
     function onedriveService($http, $resource) {
 
 
-          $http({method: 'GET', url: 'https://login.live.com/oauth20_authorize.srf?client_id={"0000000044177AA6"}&scope={"wl.signin onedrive.readonly"}&response_type=token&redirect_uri={"http://renniesb.github.io/sample_dashboard/#/"}'})
+          $http({method: 'GET', url: 'https://login.live.com/oauth20_authorize.srf?client_id={"0000000044177AA6"}&scope={"onedrive.readonly"}&response_type=token&redirect_uri={"http://renniesb.github.io/sample_dashboard/#/"}'})
         .success(function(d){ console.log( "yay" ); })
         .error(function(d){ console.log( "nope" ); });
 
