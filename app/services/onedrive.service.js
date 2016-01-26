2b5769a778a19ab04b57c3b03e5497858d8fd76e
Rennie Bevineau <rsbevineau@gmail.com>
2016-01-25 17:50:12 -0700

    request with jsonp trick

--- app/services/onedrive.service.js
+++ app/services/onedrive.service.js
@@ -1,13 +1,17 @@
 (function() {
     'use strict'
-    angular.module('demoApp').service('onedriveService', ['$http','$resource', onedriveService]);
+    angular.module('demoApp').service('onedriveService', ['$scope','$http','$resource', onedriveService]);
 
-    function onedriveService($http, $resource) {
+    function onedriveService($scope, $http, $resource) {
+
+    	$scope.OneDriveAPI = $resource('https://login.live.com/oauth20_authorize.srf?client_id={"0000000044177AA6"}&scope={"onedrive.readonly"}&response_type=token&redirect_uri={"http://renniesb.github.io/sample_dashboard/#/"}', { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
+
+    	// $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
 
 
-          $http({method: 'GET', url: 'https://login.live.com/oauth20_authorize.srf?client_id={"0000000044177AA6"}&scope={"onedrive.readonly"}&response_type=token&redirect_uri={"http://renniesb.github.io/sample_dashboard/#/"}'})
-        .success(function(d){ console.log( "yay" ); })
-        .error(function(d){ console.log( "nope" ); });
+     //      $http({method: 'GET', url: 'https://login.live.com/oauth20_authorize.srf?client_id={"0000000044177AA6"}&scope={"onedrive.readonly"}&response_type=token&redirect_uri={"http://renniesb.github.io/sample_dashboard/#/"}'})
+     //    .success(function(d){ console.log( "yay" ); })
+     //    .error(function(d){ console.log( "nope" ); });
 
 
     };
