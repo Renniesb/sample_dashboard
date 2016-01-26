a27beb362b0700a0acaf7793ab7c2aa730a42969
Rennie Bevineau <rsbevineau@gmail.com>
2016-01-25 23:00:27 -0700

    delete a link copy

--- app/services/onedrive.service.js
+++ app/services/onedrive.service.js
@@ -6,7 +6,7 @@
 
     	$scope.OneDriveAPI = $resource('https://login.live.com/oauth20_authorize.srf?client_id={"0000000044177AA6"}&scope={"onedrive.readonly"}&response_type=token&redirect_uri={"http://renniesb.github.io/sample_dashboard/#/"}', { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
 
-    $scope.OneDriveAPI = $scope.weatherAPI.get();
+    $scope.OneDriveResult = $scope.weatherAPI.get();
 
 
      //      $http({method: 'GET', url: 'https://login.live.com/oauth20_authorize.srf?client_id={"0000000044177AA6"}&scope={"onedrive.readonly"}&response_type=token&redirect_uri={"http://renniesb.github.io/sample_dashboard/#/"}'})
