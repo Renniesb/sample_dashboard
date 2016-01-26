9c15ae9ffd171d9ea19eadb4deee6a85df2cc0cb
Rennie Bevineau <rsbevineau@gmail.com>
2016-01-25 21:32:18 -0700

    delete authentication files

--- app/services/onedrive.service.js
+++ app/services/onedrive.service.js
@@ -6,7 +6,7 @@
 
     	$scope.OneDriveAPI = $resource('https://login.live.com/oauth20_authorize.srf?client_id={"0000000044177AA6"}&scope={"onedrive.readonly"}&response_type=token&redirect_uri={"http://renniesb.github.io/sample_dashboard/#/"}', { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
 
-    	// $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
+    $scope.OneDriveAPI = $scope.weatherAPI.get();
 
 
      //      $http({method: 'GET', url: 'https://login.live.com/oauth20_authorize.srf?client_id={"0000000044177AA6"}&scope={"onedrive.readonly"}&response_type=token&redirect_uri={"http://renniesb.github.io/sample_dashboard/#/"}'})
